36fa7b813be414d1b335a807636f2bfa6a94f061
giannosr <106832138+giannosr@users.noreply.github.com>
2023-12-10 13:12:42 +0200

    [Bugfix] Made convex hulls global to avoid rare segfault

--- relay.cpp
+++ relay.cpp
@@ -1,3 +1,4 @@
+#include <vector>
 #include <iostream>
 
 struct line { // y = b - ax
@@ -12,31 +13,30 @@ long double intersectX(const line &m, const line &n) {
 
 class convex_hull_trick {
 public:
-	convex_hull_trick() : j(0) {}
-	convex_hull_trick(const convex_hull_trick &C) : j(C.j) {
-		for(int i = 0; i < j; ++i) L[i] = C.L[i];
-	}
+	convex_hull_trick() : L() {}
+	convex_hull_trick& operator=(const convex_hull_trick &C) { L = C.L; return *this; }
 	
 	// insertions must be sorted by a (we need decreasing values of -a) and no parallel lines
 	void insert(const long long b, const long long a) {
-		const line m = { .b = b, .a = a };
-		if (j > 1) while (intersectX(m, L[j-1]) <= intersectX(L[j-1], L[j-2]) && --j > 1);
-		L[j] = m;
-		j++;
+		int j = L.size();
+		const line m = { b, a };
+		while (j-- > 1 && intersectX(m, L[j]) <= intersectX(L[j], L[j-1])) L.pop_back();
+		L.push_back(m);
 
 		return;
 	}
 
 	long long min(const long long x) const {
-		if (j < 2) return L[0].eval(x); // one element so far
+		const int s = L.size();
+		if (s < 2) return L[0].eval(x); // one element so far
 		// else binary search the intersection points
-		int low = 0, up = j-1, mid;
+		int low = 0, up = s - 1;
 		long double x1, x2;
 		while (low <= up) {
-			mid = (low + up)/2;
+			int mid = (low + up)/2;
 			x1 = x2 = x;
 			if (mid > 0)   x1 = intersectX(L[mid], L[mid-1]);
-			if (mid < j-1) x2 = intersectX(L[mid], L[mid+1]);
+			if (mid < s-1) x2 = intersectX(L[mid], L[mid+1]);
 
 			if (x1 <= x && x <= x2) return L[mid].eval(x);
 			if (x < x1) up  = mid - 1;
@@ -49,15 +49,14 @@ public:
 	}
 
 private:
-	line L[100005]; // Convex Hull Data Structure
-	int j;          // number of lines currently included
+	std::vector<line> L;
 	void print(std::ostream &out) const { // for debug
 		out << '{' << std::endl;
-		for (int i = 0; i < j; ++i)
-			out << "\ty = " << L[i].b << " - " << L[i].a << 'x' << std::endl;
+		for (const auto &m : L)
+			out << "\ty = " << m.b << " - " << m.a << 'x' << std::endl;
 		out << '}' << std::endl;
 	}
-};
+} C[100005]; // 10^5 + 5
 
 struct node {
 	node(const long long v, const int d, const node* const n): V(v), Dij(d), next(n) {}
@@ -66,8 +65,8 @@ struct node {
 	const node *next;
 };
 
-long long P[100005], S[100005], DP[100005]; // 10^5 + 5
-const node *L[100005];
+long long   P[100005], S[100005], DP[100005]; // 10^5 + 5
+const node* L[100005];
 bool explored[100005];
 
 
@@ -81,23 +80,23 @@ int get_n_unexplored_children(const long long v) {
 	return n_children;
 }
 
-void DFS(const long long v, convex_hull_trick* const C, const long long D) { // D = D1v, the total distance from 1 to v
+void DFS(const long long v, const long long D, const int convex_hull_index) { // D = D1v, the total distance from 1 to v
 	explored[v] = true;
 
 	if (v != 1) { // v = 1 is the base case where DP[1] = D[1] = 0 (that's why we inserted (0, 0) before calling this)
-		DP[v] = P[v] + S[v]*D + C->min(S[v]); // DP[v] = Pv + Sv Dv + min_{u ancestor of v}(DP[u] - Du Sv)
-		C->insert(DP[v], D);
+		DP[v] = P[v] + S[v]*D + C[convex_hull_index].min(S[v]); // DP[v] = Pv + Sv Dv + min_{u ancestor of v}(DP[u] - Du Sv)
+		C[convex_hull_index].insert(DP[v], D);
 	}
 
-	int i = 0, n_children = get_n_unexplored_children(v);
-	const node *n = L[v];
+	int n_children = get_n_unexplored_children(v);
+	const node *n  = L[v];
 	while(n != nullptr) {
 		if(!explored[n->V]) {
-			// don't copy C for the last child (so if there is only one child don't copy C at all -> drops complexity in list cases from O(n^2) to O(n logn))
+			// don't copy the convex hull for the last child (so if there is only one child don't copy C at all -> drops complexity in list cases from O(n^2) to O(n logn))
 			// (generally it needs to be copied because any child may modify it but it shouldn't affect other children)
-			convex_hull_trick* const H = ++i != n_children ? new convex_hull_trick(*C) : C;
-			DFS(n->V, H, D + n->Dij);
-			if (C != H) delete H;
+			int h = convex_hull_index;
+			if (--n_children) C[h = n->V] = C[convex_hull_index];
+			DFS(n->V, D + n->Dij, h);
 		}
 		n = n->next;
 	}
@@ -115,9 +114,8 @@ int main() {
 	for (int i = 2; i <= N; ++i)
 		std::cin >> P[i] >> S[i];
 
-	convex_hull_trick* const C = new convex_hull_trick;
-	C->insert(0, 0);
-	DFS(1, C, 0);
+	C[1].insert(0, 0);
+	DFS(1, 0, 1);
 
 	for (int i = 2; i < N; ++i)
 		std::cout << DP[i] << ' ';
